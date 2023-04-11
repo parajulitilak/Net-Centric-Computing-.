7b6445a1dcefe3b118d07502b060ed4c46c5fdb6
Tilak Parajuli <128390170+parajulitilak@users.noreply.github.com>
2023-04-11 09:23:42 +0545

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tilaks_Portfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
