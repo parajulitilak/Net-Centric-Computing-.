19dbf71adc7849ddc97093f6419f4068a9e01b78
Tilak Parajuli <128390170+parajulitilak@users.noreply.github.com>
2023-04-11 09:26:59 +0545

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Tilaks_Portfolio.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
